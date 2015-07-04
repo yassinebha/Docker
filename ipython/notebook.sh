be3687153848abd7c22e72aa511315183e55c747
Yassine Benhajali <yanamarji@gmail.com>
2015-07-03 23:30:56 -0400

    matlab kernel added

--- ipython/notebook.sh
+++ ipython/notebook.sh
@@ -21,4 +21,5 @@ if [ $USE_HTTP -ne 0 ]; then
   CERTFILE_OPTION=""
 fi
 
-ipython notebook --no-browser --port 8888 --ip=* $CERTFILE_OPTION --NotebookApp.password="$HASH"
+MATLAB_EXECUTABLE=/usr/bin/octave
+ipython notebook --no-browser --port 8888 --ip=* $CERTFILE_OPTION --NotebookApp.password="$HASH" --kernel=matlab_kernel
